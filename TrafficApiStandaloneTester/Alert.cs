b2397b1aa7a92f4c5c0276d6f603efb43d58cca2
Chris Britton <Christopher.L.Britton@outlook.com>
2023-06-15 20:59:38 -0400

    Initial commit of the traffic api skeleton
    Implement polling logic, poll the Washington state dot API every 10 seconds for events - print the events to the console

---
+++ TrafficApiStandaloneTester/Alert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WashingtonTrafficApi
+{
+    public class Alert
+    {
+        public int AlertID { get; set; }
+        public string HeadlineDescription { get; set; }
+        public string EventCategory { get; set; }
+        public string Priority { get; set; }
+        public DateTime LastUpdatedTime { get; set; }
+    }
+}
---
+++ TrafficApiStandaloneTester/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficApiStandaloneTester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Poller poller = new Poller();
+            poller.StartPoller();
+            Console.Read();
+        }
+    }
+}
